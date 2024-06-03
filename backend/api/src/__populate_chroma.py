6bcd910ad473b0b82beff162f420db95e99fa4d0
antoniczapski <antoni.krzysztof.czapski@gmail.com>
2024-06-04 00:02:47 +0200

    Docker deployment - backend & frontend

---
+++ backend/api/src/__populate_chroma.py
@@ -0,0 +1,43 @@
+import pathlib
+
+import bs4
+from dotenv import load_dotenv
+from langchain_community.document_loaders.html_bs import BSHTMLLoader
+from langchain_community.document_loaders.pdf import PDFMinerLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from chroma import get_chroma_client
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    DATA_DIR = pathlib.Path(__file__).parent / "data"
+
+    # text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    #     model_name="gpt-3.5-turbo",
+    #     chunk_size=1000,
+    #     chunk_overlap=200,
+    # )
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=200,
+    )
+
+    documents = []
+    for pdf in map(str, DATA_DIR.glob("*.pdf")):
+        documents.extend(PDFMinerLoader(pdf).load())
+    for html in DATA_DIR.glob("*.html"):
+        loader = BSHTMLLoader(
+            html,
+            bs_kwargs=dict(
+                features="html.parser",
+                parse_only=bs4.SoupStrainer("article"),
+            ),
+        )
+        documents.extend(loader.load())
+
+    documents = text_splitter.split_documents(documents)
+
+    chroma = get_chroma_client()
+    chroma.add_documents(documents)
---
+++ backend/api/src/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Request
+import uvicorn
+
+from receptionata import receptionata_router
+
+app = FastAPI()
+app.include_router(receptionata_router, prefix="/receptionata")
+
+# Global variable to store the last request
+last_request = None
+
+# For debugging purposes - logging of all incoming requests
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    global last_request
+    last_request = f"Incoming request: {request.method} {request.url}"
+    print(last_request)
+    response = await call_next(request)
+    return response
+
+@app.get("/last_request")
+async def get_last_request():
+    return {"last_request": last_request}
+
+def main():
+    uvicorn.run(
+        app=app,
+        loop="uvloop",
+        port=8000,
+        host="0.0.0.0",
+    )
+
+if __name__ == "__main__":
+    main()
